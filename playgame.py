6badf1976b88f295cc85d1946ce94da7cb700dfc
Nate Kim <enemeski@gmail.com>
2024-04-19 18:16:40 -0700

    Boat placement working now

--- playgame.py
+++ playgame.py
@@ -8,8 +8,9 @@ SHIPS_NAMES = ["aircraft carrier", "battleship", "cruiser", "submarine", "destro
 MIRRORED_BOARD_LABELS = True
 STR_TO_INT = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9}
 INT_TO_STR = {0:"A",1:"B",2:"C",3:"D",4:"E",5:"F",6:"G",7:"H",8:"I",9:"J"}
+PIECE_CHAR = '#'
 
-
+# Set up 10x10 grid with row and column labels
 def initialize_board():
   # 1-10 at the top of the board
   col_titles = [' '] + [str(i) for i in range(GRID_SIZE)]
@@ -27,7 +28,7 @@ def validate_placement(c: str, i: int) -> bool:
   return False
 
 # Takes coordinate and ship size, returns list of possible swing coordinates
-def get_allowed_swing_points(c: str, i: int, ship_size: int) -> list[str]:
+def get_allowed_swing_points(c: str, i: int, ship_size: int) -> tuple[str]:
   possible_positions = []
   # The offset of 2 accounts for only needing to be ship_size-1 spots away from the anchor
   # If right swing is possible, append the right swing coordinate
@@ -57,7 +58,7 @@ class BoardState:
 
   def place_anchor(self: "BoardState", c: str, i: int) -> "BoardState":
     row_choice = STR_TO_INT[c]
-    self.state[row_choice+1][i+1] = '+'
+    self.state[row_choice+1][i+1] = PIECE_CHAR
     return self
 
   def place_ship(self):
@@ -65,6 +66,7 @@ class BoardState:
     swing_point = None # String ex. "A1"
     num_ships_placed = 0
     for ship in SHIPS_NAMES:
+
       # Place anchor point
       while(anchor_point == None):
         print(self)
@@ -73,25 +75,41 @@ class BoardState:
         # Check input length
         if (len(player_choice) != 2): continue
         # Check input validity
-        coordinate_info = coordinate_to_tuple(player_choice)
-        if (validate_placement(coordinate_info[0], coordinate_info[1])):
+        # TO-DO: make lowercase char value as coordinate work
+        anchor_coordinate_info = coordinate_to_tuple(player_choice)
+        if (validate_placement(anchor_coordinate_info[0], anchor_coordinate_info[1])):
           anchor_point = player_choice
-          num_ships_placed += 1
+
       # Place the anchor point on the board and show it to the player
-      self.place_anchor(coordinate_info[0], coordinate_info[1])
+      self.place_anchor(anchor_coordinate_info[0], anchor_coordinate_info[1])
+      # TO-DO: show the possible secondary points on the board using ?'s
       print(self)
+
       # Get possible secondary points
-      allowed_swing_points = get_allowed_swing_points(coordinate_info[0], coordinate_info[1], SHIPS_SIZES[ship])
+      allowed_swing_points = get_allowed_swing_points(anchor_coordinate_info[0], anchor_coordinate_info[1], SHIPS_SIZES[ship])
       while (swing_point == None):
         print("Choose a second point for your " + ship + ", which has size " + str(SHIPS_SIZES[ship]) + ".")
-        # Should make it possible to reset the anchor
+        # TO-DO: make it possible to reset the anchor (maybe 00 -> reset anchor)
         print("The options are: " + " ".join(allowed_swing_points))
         player_choice = input("Enter your choice here: ")
-        if (len(player_choice) == 2 and validate_placement(player_choice[0], player_choice[1])):
-          break
-          possiblePositions = 1
-          anchor_point = player_choice
-          num_ships_placed += 1
+        # TO-DO: make sure ships have no overlap
+        if (player_choice in allowed_swing_points):
+          swing_point = player_choice
+      
+      # Place ship onto board
+      swing_coordinate_info = coordinate_to_tuple(player_choice)
+      # Anchor and swing x & y aliases, all in integer form
+      a_x, a_y = (STR_TO_INT[anchor_coordinate_info[0]], anchor_coordinate_info[1])
+      s_x, s_y = (STR_TO_INT[swing_coordinate_info[0]], swing_coordinate_info[1])
+      if a_y == s_y:  # Horizontal line
+        for x in range(min(a_x, s_x), max(a_x, s_x)):
+          self.state[x+1][a_y+1] = PIECE_CHAR # +1 to account for board labels
+      if a_x == s_x:  # Vertical line
+        for y in range(min(a_y, s_y), max(a_y, s_y)):
+          self.state[a_x+1][y+1] = PIECE_CHAR # +1 to account for board labels
+      
+      print(self)
+      break
 
 
 
