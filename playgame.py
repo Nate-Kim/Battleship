1df113f919ee5bbf03daa92514833ecbf1e142c9
Nate Kim <enemeski@gmail.com>
2024-04-20 11:09:52 -0700

    adversarial board random generation, console clearing for player input, added functionality that catches anchor points with no available swing points

--- playgame.py
+++ playgame.py
@@ -1,3 +1,6 @@
+import random
+import os
+
 GRID_SIZE = 10
 SHIPS_SIZES = {"aircraft carrier": 5, 
               "battleship": 4, 
@@ -8,6 +11,9 @@ SHIPS_NAMES = ["aircraft carrier", "battleship", "cruiser", "submarine", "destro
 STR_TO_INT = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9}
 INT_TO_STR = {0:"A",1:"B",2:"C",3:"D",4:"E",5:"F",6:"G",7:"H",8:"I",9:"J"}
 PIECE_CHAR = '#'
+
+def clear_console():
+  os.system('cls' if os.name == 'nt' else 'clear')
 
 # Takes a coordinate and ensures it lies in board range
 def coordinate_in_board(c: str, i: int) -> bool:
@@ -41,22 +47,22 @@ class BoardState:
     return self
   
   # Takes coordinate and ship size, returns list of possible swing coordinates
-  def get_allowed_swing_points(self, anchor_row: str, anchor_col: int, ship_size: int) -> tuple[str]:
+  def get_allowed_swing_points(self, anchor_row: str, anchor_col: int, ship_size: int) -> list[str]:
     possible_positions = []
     swing_down_allowed = True
     swing_right_allowed = True 
     swing_up_allowed = True
     swing_left_allowed = True
     # The offset of 1 accounts for only needing to be ship_size-1 spots away from the anchor
-    right_swing_point = anchor_col+ship_size-1
-    down_swing_point = STR_TO_INT[anchor_row]+ship_size-1
-    left_swing_point = anchor_col-ship_size+1
-    up_swing_point = STR_TO_INT[anchor_row]-ship_size+1
+    right_swing_point = anchor_col+(ship_size-1)
+    down_swing_point = STR_TO_INT[anchor_row]+(ship_size-1)
+    left_swing_point = anchor_col-(ship_size-1)
+    up_swing_point = STR_TO_INT[anchor_row]-(ship_size-1)
     
     # If down swing is in the grid
     if (down_swing_point <= 9): 
       # If a coordinate in the candidate ship space is taken, the swing is not allowed
-      for n in range(STR_TO_INT[anchor_row]+1, down_swing_point-1): # Don't want to count anchor as a different ship
+      for n in range(STR_TO_INT[anchor_row]+1, down_swing_point+1): # Don't want to count anchor as a different ship
         if (self.state[n][anchor_col] != '~'):
           swing_down_allowed = False
     else: 
@@ -65,7 +71,7 @@ class BoardState:
     # If right swing is in the grid
     if (right_swing_point <= 9):
       # If a coordinate in the candidate ship space is taken, the swing is not allowed
-      for n in range(anchor_col+1, right_swing_point-1):
+      for n in range(anchor_col+1, right_swing_point+1):
         if (self.state[STR_TO_INT[anchor_row]][n] != '~'):
           swing_right_allowed = False
     else: 
@@ -99,48 +105,112 @@ class BoardState:
   def place_ship(self, ship: str):
     anchor_point = None # String ex. "A1"
     swing_point = None # String ex. "A1"
-    # Place anchor point
-    while(anchor_point == None):
+    invalid = False
+    while True:
+      # Place anchor point
+      while anchor_point == None:
+        clear_console()
+        if invalid: print("Invalid anchor point.")
+        print(self)
+        print("Choose an anchor point (ex. A1) for your " + ship + ", which has size " + str(SHIPS_SIZES[ship]) + ".")
+        player_choice = input("Enter your choice here: ")
+        # Check input length
+        if (len(player_choice.strip()) != 2): continue
+        # Check input validity
+        anchor_coordinate_info = coordinate_to_tuple(player_choice.upper().strip())
+        if (coordinate_in_board(anchor_coordinate_info[0], anchor_coordinate_info[1])):
+          anchor_point = player_choice
+
+      # Place the anchor point on the board
+      self.place_anchor(anchor_coordinate_info[0], anchor_coordinate_info[1])
+      # If there are no possible swing points from the chosen anchor
+      if len(self.get_allowed_swing_points(anchor_coordinate_info[0], anchor_coordinate_info[1], SHIPS_SIZES[ship])) == 0:
+        invalid = True
+        # Set the chosen anchor back to an empty space
+        self.state[STR_TO_INT[anchor_coordinate_info[0]]][anchor_coordinate_info[1]] = '~'
+        # Reset anchor point
+        anchor_point = None
+        continue
+      clear_console()
       print(self)
-      print("Choose an anchor point (ex. A1) for your " + ship + ", which has size " + str(SHIPS_SIZES[ship]) + ".")
-      player_choice = input("Enter your choice here: ")
-      # Check input length
-      if (len(player_choice) != 2): continue
-      # Check input validity
-      # TO-DO: make lowercase char value as coordinate work
-      anchor_coordinate_info = coordinate_to_tuple(player_choice)
-      if (coordinate_in_board(anchor_coordinate_info[0], anchor_coordinate_info[1])):
-        anchor_point = player_choice
 
-    # Place the anchor point on the board and show it to the player
-    self.place_anchor(anchor_coordinate_info[0], anchor_coordinate_info[1])
-    # TO-DO: show the possible secondary points on the board using ?'s
-    print(self)
+      # Get possible secondary points (orientations that are in bounds and do not overlap other ships)
+      allowed_swing_points = self.get_allowed_swing_points(anchor_coordinate_info[0], anchor_coordinate_info[1], SHIPS_SIZES[ship])
+      while (swing_point == None):
+        print("Choose a second point for your " + ship + ", which has size " + str(SHIPS_SIZES[ship]) + ".")
+        # TO-DO: make it possible to reset the anchor (maybe 00 -> reset anchor)
+        print("The options are: " + " ".join(allowed_swing_points))
+        player_choice = input("Enter your choice here: ")
+        if (player_choice.upper().strip() in allowed_swing_points):
+          swing_point = player_choice.upper().strip()
+        else:
+          clear_console()
+          print("Please choose a valid secondary point.")
+          print(self)
 
-    # Get possible secondary points
-    allowed_swing_points = self.get_allowed_swing_points(anchor_coordinate_info[0], anchor_coordinate_info[1], SHIPS_SIZES[ship])
-    while (swing_point == None):
-      print("Choose a second point for your " + ship + ", which has size " + str(SHIPS_SIZES[ship]) + ".")
-      # TO-DO: make it possible to reset the anchor (maybe 00 -> reset anchor)
-      print("The options are: " + " ".join(allowed_swing_points))
-      player_choice = input("Enter your choice here: ")
-      # TO-DO: make sure ships have no overlap
-      if (player_choice in allowed_swing_points):
-        swing_point = player_choice
-    
-    # Place ship onto board
-    swing_coordinate_info = coordinate_to_tuple(player_choice)
-    # Anchor and swing x & y aliases, all in integer form
-    a_x, a_y = (STR_TO_INT[anchor_coordinate_info[0]], anchor_coordinate_info[1])
-    s_x, s_y = (STR_TO_INT[swing_coordinate_info[0]], swing_coordinate_info[1])
-    if a_y == s_y:  # Horizontal orientation
-      for x in range(min(a_x, s_x), max(a_x, s_x)+1):
-        self.state[x][a_y] = PIECE_CHAR
-    if a_x == s_x:  # Vertical orientation
-      for y in range(min(a_y, s_y), max(a_y, s_y)+1):
-        self.state[a_x][y] = PIECE_CHAR
+      # Place ship onto board
+      swing_coordinate_info = coordinate_to_tuple(player_choice.upper().strip())
+      # Anchor and swing x & y aliases, all in integer form
+      a_x, a_y = (STR_TO_INT[anchor_coordinate_info[0]], anchor_coordinate_info[1])
+      s_x, s_y = (STR_TO_INT[swing_coordinate_info[0]], swing_coordinate_info[1])
+      if a_y == s_y:  # Horizontal orientation
+        for x in range(min(a_x, s_x), max(a_x, s_x)+1):
+          self.state[x][a_y] = PIECE_CHAR
+      if a_x == s_x:  # Vertical orientation
+        for y in range(min(a_y, s_y), max(a_y, s_y)+1):
+          self.state[a_x][y] = PIECE_CHAR
+
+      clear_console()
+      print(self)
+      break
+
+  # Return a random anchor coordinate (for base adversary implementation)
+  def random_coordinate(self):
+    coordinate_value = '#'
+    while coordinate_value == '#':
+      random_row = random.randint(0, GRID_SIZE - 1)
+      random_col = random.randint(0, GRID_SIZE - 1)
+      coordinate_value = self.state[random_row][random_col]
+    return INT_TO_STR[random_row] + str(random_col)
+
+  # Randomly place down a single ship
+  def randomly_place_ship(self, ship: str):
+    anchor_point = None
+    swing_point = None
+
+    while True:
+      # Ensured to NOT be on top of a currently placed ship
+      anchor_point = self.random_coordinate()
+      anchor_coordinate_info = coordinate_to_tuple(anchor_point)
+
+      # Place the anchor point on the board
+      self.place_anchor(anchor_coordinate_info[0], anchor_coordinate_info[1])
+      # If there are no possible swing points from the chosen anchor
+      if len(self.get_allowed_swing_points(anchor_coordinate_info[0], anchor_coordinate_info[1], SHIPS_SIZES[ship])) == 0:
+        # Set the chosen anchor back to an empty space
+        self.state[STR_TO_INT[anchor_coordinate_info[0]]][anchor_coordinate_info[1]] = '~'
+        # Reset anchor point
+        anchor_point = None
+        continue
+
+      # Get possible secondary points (orientations that are in bounds and do not overlap other ships)
+      allowed_swing_points = self.get_allowed_swing_points(anchor_coordinate_info[0], anchor_coordinate_info[1], SHIPS_SIZES[ship])
+      random_choice = allowed_swing_points[random.randint(0, len(allowed_swing_points) - 1)]
+      swing_point = random_choice
       
-    print(self)
+      # Place ship onto board
+      swing_coordinate_info = coordinate_to_tuple(swing_point)
+      # Anchor and swing x & y aliases, all in integer form
+      a_x, a_y = (STR_TO_INT[anchor_coordinate_info[0]], anchor_coordinate_info[1])
+      s_x, s_y = (STR_TO_INT[swing_coordinate_info[0]], swing_coordinate_info[1])
+      if a_y == s_y:  # Horizontal orientation
+        for x in range(min(a_x, s_x), max(a_x, s_x)+1):
+          self.state[x][a_y] = PIECE_CHAR
+      if a_x == s_x:  # Vertical orientation
+        for y in range(min(a_y, s_y), max(a_y, s_y)+1):
+          self.state[a_x][y] = PIECE_CHAR
+      
+      break
 
   # Add labels to the board representation
   def __str__(self):
@@ -150,8 +220,11 @@ class BoardState:
 
 def main():
   player_grid = BoardState()
-  player_grid.place_ship(SHIPS_NAMES[0])
-  player_grid.place_ship(SHIPS_NAMES[1])
+  for ship in SHIPS_NAMES:
+    player_grid.place_ship(ship)
+  AI_grid = BoardState()
+  for ship in SHIPS_NAMES:
+    AI_grid.randomly_place_ship(ship)
 
 if __name__ == "__main__":
   main()
